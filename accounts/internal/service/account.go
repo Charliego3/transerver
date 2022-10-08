830cfbf54755e722374c5d1ebd23e19f1a107284
Charlie <whimthen@gmail.com>
2022-10-08 10:19:01 +0800

    proto files

--- accounts/internal/service/account.go
+++ accounts/internal/service/account.go
@@ -1,14 +1,19 @@
 package service
 
 import (
+	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/transerver/accounts/internal/biz"
+	"github.com/transerver/protos/acctspb"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AccountService struct {
-	// TODO: Unimplemented pb
+	acctspb.UnimplementedAccountServiceServer
+
 	usecase *biz.AccountUsecase
 	logger  *zap.Logger
 }
@@ -18,9 +23,17 @@ func NewAccountService(usecase *biz.AccountUsecase, logger *zap.Logger) *Account
 }
 
 func (g *AccountService) RegisterGRPC(s *grpc.Server) {
-
+	acctspb.RegisterAccountServiceServer(s, g)
 }
 
 func (g *AccountService) RegisterHTTP(s *runtime.ServeMux) error {
-	return nil
+	return acctspb.RegisterAccountServiceHandlerServer(context.Background(), s, g)
+}
+
+func (g *AccountService) Register(context.Context, *acctspb.RegisterRequest) (*acctspb.RegisterReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
+}
+
+func (g *AccountService) Login(context.Context, *acctspb.LoginRequest) (*acctspb.LoginReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
 }
